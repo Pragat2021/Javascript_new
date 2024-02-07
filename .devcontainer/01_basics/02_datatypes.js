8905f62dabbc64e087d981b428fc4275e8377ebf
Pragat2021 <119549961+Pragat2021@users.noreply.github.com>
2024-02-07 19:10:50 +0000

    datatypes done

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict";//treat all js code as newer version or new code version format
+
+//alert(3+3) this is invalid as we r usong node js not browser; this is acceptable only in browser
+// we avoid semi colon in js
+console.log(3+3);// code must be highly readable
+console.log("Hello world");
+//tc39 ecma make standards for java script
+//mdn is also one of the documentation for javascript
+
+let name="Pragat"//string datatype
+let age=18//int datatype
+let isloggedIn = false//boolean datatype
+// number=>2 to power 53
+//bigint => for veru big int
+//string => ""
+// boolean => true/false
+// null => stand alone value as well as a datatype
+//undefined => value isin't declared
+// null is value which is not undefined but it's empty
+//symbol => identify uniqueness mainly used in react  
+// THESE ARE PREMITIVE DATA TYPES
+
+
+// NOW Come OBJECTS
+
+console.log([typeof age,typeof null,typeof undefined]);
+//Note=> null is a object datatype and undefined is itself a datatype (undefined)
+//OUTPUT => 
+/*6
+Hello world
+[ 'number', null, undefined ]
+*/
+
+
+
