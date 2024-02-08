0d529cc12de0cd405f363528a1cb786584cdb3bf
Pragat2021 <119549961+Pragat2021@users.noreply.github.com>
2024-02-08 14:54:52 +0000

    conversion operation are completed

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -44,3 +44,52 @@ let nos =33
 let samplestring = String(nos)
 console.log(samplestring);//33 answer
 console.log(typeof samplestring);
+
+
+//*******Operations ************ */
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+// output = -3
+console.log("Vrious Operator in Javascript");
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2**3);//power
+console.log(2/3);
+console.log(2%3);
+
+//***Contatenation / join 2 strings */
+let str1 = "Pragat "
+let str2= "Shukla"
+let str3 = str1 + str2
+//console.log("value of str3 is:" str3); = > This statement gives error in javascript
+// to avoid this we use
+console.log("value of str3 is :");
+console.log(str3);
+
+console.log("1"+"2");// These both will give 12 output
+console.log("1"+2);
+// now lets see some new behaiour of our javascript
+console.log("1" +2 +2); //122
+console.log(1 + 2+"2");//32
+//there is a guidline for to premitive (convert to prefered type) these are given by ECMA guillines
+console.log(3 + 4 *5 % 3);//we do not use these types of code in javascript as why to confuse our tester ; output = > 5
+
+//***TRicky Conversion
+console.log(+true);//this is possible
+//console.log(true+); => This is not possible
+console.log(+"");//=> output is 0 
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+console.log([num1,num2,num3]);
+//This is good but not preferd in industry
+
+let gamecounter = 100
+++gamecounter; // ++ gamecounter and gamecounter++ both are acceptable ++gamecounter increment themn use while gamecounter++ is use and then incremental 
+console.log(gamecounter);
+//Link to study
+//https://developer.mozilla.org/en-US/docs/Glossary/Type_Conversion
