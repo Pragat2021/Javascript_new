42c245b93d72dcb3d7e64f13ca5cf91e24a0176a
Pragat2021 <119549961+Pragat2021@users.noreply.github.com>
2024-02-09 16:56:28 +0000

    not needed

--- .devcontainer/08_dates.js
+++
@@ -1,3 +0,0 @@
-// january 1 1970 is start time ; Temporal is a api used for date and time AT Prese time is log pain in our javascript
-let mydate = new Date() // created an instance
-console.log(mydate.toString());
