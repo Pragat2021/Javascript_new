20ba1ca7d0a0efd083b04854a4ad2b2b558096c8
Pragat2021 <119549961+Pragat2021@users.noreply.github.com>
2024-02-09 17:31:50 +0000

    Time is the Biggest teacher!!

---
+++ .devcontainer/01_basics/08_date.js
@@ -0,0 +1,41 @@
+let myDate = new Date()
+//here myDate is object and Date()is a function
+// console.log(myDate.getDay());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getMonth());
+// console.log(myDate.getTime());
+// console.log(myDate.getHours());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getSeconds());
+// console.log(myDate.getUTCDate());
+// console.log(myDate.getUTCFullYear());
+// 5
+// 2024
+// 1
+// 1707498510186
+// 17
+// 8
+// 30
+// 9
+// 2024
+// //DATe method
+// console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log("1st way of date declaration");
+let mycreateddate = new Date(2023,0 ,23)//declaration of date 
+// console.log(mycreateddate.toLocaleString());
+
+// console.log("2nd way to declare date");
+// let date2= new Date(01,14,2023)
+
+//*********TIME STAMP************* */
+
+let timestamp = new Date()
+//  console.log(timestamp);
+//  console.log(mycreateddate.getTime());
+//  console.log(Math.floor(Date.now()/1000));
+
+//  console.log(`${myDate().getDate} and the time ${myDate().getTime}`);
+  
