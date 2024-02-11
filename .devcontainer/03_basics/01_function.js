342d2e822d4fbb89284124012cd8418c3b4f79f3
Pragat2021 <119549961+Pragat2021@users.noreply.github.com>
2024-02-11 10:10:10 +0000

    functions in javascript

---
+++ .devcontainer/03_basics/01_function.js
@@ -0,0 +1,21 @@
+// function definition
+function sayMyname(){
+console.log("p");
+console.log("R");
+console.log("A");
+console.log("G");
+console.log("A");
+console.log("T");
+
+}
+
+//function call
+
+// sayMyname => only function name
+sayMyname()
+
+function add2numbers(num1 ,num2)
+{
+ console.log(num1+num2);   
+}
+add2numbers(7,5)
