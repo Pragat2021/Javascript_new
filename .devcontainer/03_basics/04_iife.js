e12a7eabccbdeb1367ca1b4d561d869427b44ee7
Pragat2021 <119549961+Pragat2021@users.noreply.github.com>
2024-02-12 06:47:51 +0000

    iife =>Immediately invoked Function Expression

---
+++ .devcontainer/03_basics/04_iife.js
@@ -0,0 +1,32 @@
+// Immediately Invoked Function Expressions (IIIFE)
+//  when we want that as we write or function and it could immmediately run 
+//we donot need pollution in our global scope
+
+(function hobby ()
+{
+    console.log("Database connected");
+})();
+
+// ()()  => TO RESOLVE FROM OF POLLUTION FROM GLOBAL SCOPE WE USE IIFE IN WHICH A BRACKETS are ladden outside the function and function is written inside ()  these braces  and always apply "; " after the function is over
+
+
+// hobby()
+// Use arrows to implement a iife 
+( ()=> {//unnamed iiife
+    console.log("Hey Its Dbms");
+})();
+
+//Named iiife 
+( (name)=> {
+    console.log(`${name} is connected to dbms `);
+})("Pragat");
+
+//IIIFE inside iiife => Nested IIIFE
+((Love) => {
+    console.log(`${Love} loves Eating`);
+
+    ((food) => {
+        console.log(`He Loves eating ${food}`);
+    })("Samosa");
+
+})("Pragat");
