829bdabf3a3ed3c0f6dc5e95c70a7c42a42bfa32
Pragat2021 <119549961+Pragat2021@users.noreply.github.com>
2024-02-14 19:53:29 +0000

    for each loop

--- .devcontainer/05_itterations/05_foreach.js
+++ .devcontainer/05_itterations/05_foreach.js
@@ -2,3 +2,40 @@ const code = ["js" , "c" , "python" , "rubuy"]
 code.forEach
 
 // here this is cal back function no need to give akues
+// code.forEach(item)=>
+// {
+//     console.log(item);
+// }
+
+// function printme(item)
+// {
+//     console.log(item);
+// }
+// code.forEach(printme)
+
+// code.forEach((item , index , arr ) =>
+// {
+//     console.log(item , index , arr);
+// })
+
+
+// objects in foreach
+
+const mycode = [
+    {
+        lang:"javascript",
+        file:"Js"
+    },
+    {
+        lang:"Python",
+        file:"py"
+    },
+    {
+        lang:"c++",
+        file:"cpp"
+    }
+]
+mycode.forEach((item) =>{
+
+    console.log(item.lang , item.file);
+})
